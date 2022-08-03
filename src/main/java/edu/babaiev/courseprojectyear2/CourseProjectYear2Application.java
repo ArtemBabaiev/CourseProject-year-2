9ef4573d69ea1dfcae5d30ba3393ebecf71bd23f
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-08-03 16:49:29 +0300

    spring boot new

---
+++ src/main/java/edu/babaiev/courseprojectyear2/CourseProjectYear2Application.java
@@ -0,0 +1,13 @@
+package edu.babaiev.courseprojectyear2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectYear2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectYear2Application.class, args);
+    }
+
+}
