a335af05bcaedfd3cae805722bdf5d6fa36ac2a0
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-08-14 22:52:50 +0300

    config

---
+++ src/main/java/edu/babaiev/libr/Configuration.java
@@ -0,0 +1,17 @@
+package edu.babaiev.libr;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * @author artem
+ * @version: 1.0.0
+ * @project CourseProject-year-2
+ * @date 14.08.2022 22:39
+ * @class Configuration
+ */
+@org.springframework.context.annotation.Configuration
+@EnableJpaRepositories(basePackages = "edu.babaiev.libr.repository.sql")
+@EnableMongoRepositories(basePackages = "edu.babaiev.libr.repository.mongo")
+public class Configuration {
+}
