92734d2cbd931359ededac1ac8697bae63c06362
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-08-14 22:27:03 +0300

    package rename

--- src/main/java/edu/babaiev/libr/CourseProjectYear2Application.java
+++ src/main/java/edu/babaiev/libr/CourseProjectYear2Application.java
@@ -1,4 +1,4 @@
-package edu.babaiev.courseprojectyear2;
+package edu.babaiev.libr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/edu/babaiev/libr/CourseProjectYear2ApplicationTests.java
+++ src/test/java/edu/babaiev/libr/CourseProjectYear2ApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.babaiev.courseprojectyear2;
+package edu.babaiev.libr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
