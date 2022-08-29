15e44e36e45946f02f6c1fa7f69812d9cecc250a
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-08-30 01:27:18 +0300

    Form class for category choice

---
+++ src/main/java/edu/babaiev/libr/form/CategoryChooseForm.java
@@ -0,0 +1,43 @@
+package edu.babaiev.libr.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * @author artem
+ * @version: 1.0.0
+ * @project CourseProject-year-2
+ * @date 26.08.2022 23:55
+ * @class CategoryChooseForm
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryChooseForm {
+    private String category;
+
+    @Override
+    public String toString() {
+        return "CategoryChooseForm{" +
+                "category='" + category + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryChooseForm that = (CategoryChooseForm) o;
+        return category.equals(that.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category);
+    }
+}
