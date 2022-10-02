991703335e36322d29d1a50262dfa04d84226190
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-10-03 02:04:48 +0300

    rework starting

---
+++ src/test/java/edu/chnu/library/CourseProjectYear2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.chnu.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectYear2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
